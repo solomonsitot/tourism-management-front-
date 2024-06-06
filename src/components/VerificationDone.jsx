c6417321c78df745d7030603f570e4adb81cfd59
solomon sitotaw <Solotest922@gmail.com>
2024-06-07 02:59:34 +0300

    modifications

--- src/components/VerificationDone.jsx
+++ src/components/VerificationDone.jsx
@@ -12,7 +12,8 @@ import "react-toastify/dist/ReactToastify.css";
 function VerificationDone() {
   const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
   const { verifyToken } = useParams();
-  const [results, setResults] = useState("");
+  const [stat, setstat] = useState(false);
+  const [body, setBody] = useState([]);
   const navigate = useNavigate();
 
   async function verifyEmail() {
@@ -21,47 +22,58 @@ function VerificationDone() {
       const response = await axios.put(
         `${BACKEND_URL}/user/verify-email/${verifyToken}`
       );
-      const { message, body } = response.data;
+      const { message, body, status } = response.data;
       console.log(body);
-      toast.success(
-        <div>
-          <FontAwesomeIcon icon={faCheckCircle} /> {message}
-        </div>
-      );
-
-      setTimeout(() => {
-        if (body && body.role) {
-          navigate(`/cridentials/${body.role}/${body._id}`);
-        } else {
-          toast.error(
-            <div>
-              <FontAwesomeIcon icon={faExclamationTriangle} /> Error in response
-              body
-            </div>
-          );
-        }
-      }, 3000);
+      toast.success(message);
+      setstat(status);
+      setBody(body);
     } catch (err) {
-      toast.error(
-        <div>
-          <FontAwesomeIcon icon={faExclamationTriangle} /> {err.message}
-        </div>
-      );
+      toast.error(err.message);
     }
   }
 
+  async function changeSite() {
+    try {
+      if (body && body.role) {
+        navigate(`/cridentials/${body.role}/${body._id}`);
+      } else {
+        toast.error(
+          <div>
+            <FontAwesomeIcon icon={faExclamationTriangle} /> Error in response
+            body
+          </div>
+        );
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
       <div className="bg-white shadow-md rounded-lg p-6 md:p-8">
         <p className="text-xl font-semibold mb-4">Click the Button to Verify</p>
         <button
           onClick={verifyEmail}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded flex items-center"
+          className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded flex items-center ${
+            stat ? "hidden" : "block"
+          }`}
         >
           <FontAwesomeIcon icon={faCheckCircle} className="mr-2" />
           Click here
         </button>
+
+        <button
+          onClick={changeSite}
+          className={`bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded flex items-center ${
+            stat ? "block" : "hidden"
+          }
+          }`}
+        >
+          <FontAwesomeIcon icon={faCheckCircle} className="mr-2" />
+          Continue
+        </button>
       </div>
+
       <ToastContainer />
     </div>
   );
